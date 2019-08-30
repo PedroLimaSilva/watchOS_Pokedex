0e680acab80e72d53949d4a2346ca7ff6f44b80c
Pedro Lima e Silva <pedrocaslsilva403@gmail.com>
2019-08-30 08:00:22 +0100

    Search pokemon list
    Update navigation bar with current search term

--- Pokedex WatchKit Extension/PokemonList.swift
+++ Pokedex WatchKit Extension/PokemonList.swift
@@ -9,15 +9,34 @@
 import SwiftUI
 
 struct PokemonList: View {
+    @State var searchTerm = ""
+    func clearSearch() {
+        searchTerm = ""
+    }
     var body: some View {
         List {
-            ForEach(pokemonData){ pokemon in
-                NavigationLink(destination: PokemonDetail(pokemon: pokemon)){
-                     PokemonRow(pokemon: pokemon)
+            GeometryReader { geometry in
+                HStack(alignment: .center) {
+                    TextField("Search", text: self.$searchTerm)
+                        .frame(width: geometry.size.width - 15.0, height: geometry.size.height)
+                        .position(x: geometry.size.width/2 - 15.0, y: geometry.size.height/2)
+                    Button(action: self.clearSearch) {
+                        Image("clear")
+                            .resizable()
+                            .frame(width: 30.0, height: 30.0)
+                            .position(x: geometry.size.width - 82.5, y: geometry.size.height/2)
+                            .opacity(0.5)
+                    }
+                }
+            }
+            
+            ForEach(pokemonData.filter{ self.searchTerm.isEmpty ? true : $0.name.localizedCaseInsensitiveContains(self.searchTerm) }){ pokemon in
+                NavigationLink(destination: PokemonDetail(pokemon: pokemon)) {
+                    PokemonRow(pokemon: pokemon)
                 }
             }
         }
-        .navigationBarTitle(Text("All Pokemon"))
+        .navigationBarTitle(Text(self.searchTerm.isEmpty ? "All Pokemon" : "\"\(self.searchTerm)\""))
     }
 }
 
